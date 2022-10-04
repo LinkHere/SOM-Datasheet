1b828ec22fd08dbae5322e4d1d75d43e40061f90
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-04 11:53:37 +0800

    Update Main_Page.py

--- Main/Main_Page.py
+++ Main/Main_Page.py
@@ -1,3 +1,29 @@
 import streamlit as st
 
-st.title("<=== Select a Year Level")
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+st.markdown(f"""
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+""", unsafe_allow_html=True)
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+
+conn = connect(credentials=credentials)
+
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["yrl1"]
+
+rows = run_query(f'SELECT Last_Name FROM "{sheet_url}"')
+
+st.write(len(rows))
