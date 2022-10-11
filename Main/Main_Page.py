8ec2047b5e5de2aed478ad44d78172a710963fad
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-11 09:23:00 +0800

    Update Main_Page.py

--- Main/Main_Page.py
+++ Main/Main_Page.py
@@ -35,7 +35,6 @@ st.markdown(f"""
         <div class="card-header">
             Featured
         </div>
-        
         <ul class="list-group list-group-flush">
             <li class="list-group-item">An item</li>
             <li class="list-group-item">A second item</li>
