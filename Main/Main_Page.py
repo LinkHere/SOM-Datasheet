aa26f4735f04228a809b15a918f1699ce757311d
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-12 08:43:47 +0800

    Update Main_Page.py

--- Main/Main_Page.py
+++ Main/Main_Page.py
@@ -52,8 +52,8 @@ st.markdown(f"""
   <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">1st Year Students</h5>
+        <p class="card-text">Found: <span class="badge bg-secondary">{len(rows1)}</span></p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
