d6a9a9ce6754226aedf8957bf2be4ea3eb49b464
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-12 08:51:43 +0800

    Update Main_Page.py

--- Main/Main_Page.py
+++ Main/Main_Page.py
@@ -54,7 +54,7 @@ st.markdown(f"""
       <div class="card-body">
         <h5 class="card-title" style="color: #777;">1st Year Students</h5>
         <p class="card-text">Found: <span class="badge bg-danger">{len(rows1)}</span></p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="/Year_Level_1" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
