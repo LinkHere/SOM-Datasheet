224557a8a87e92cbbd9bbda13fcb941c73c10c22
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-12 09:06:07 +0800

    Update Main_Page.py

--- Main/Main_Page.py
+++ Main/Main_Page.py
@@ -54,7 +54,7 @@ st.markdown(f"""
       <div class="card-body">
         <h5 class="card-title" style="color: #777;">1st Year Students</h5>
         <p class="card-text">Found: <span class="badge bg-danger">{len(rows1)}</span></p>
-        <p><strong><<< Click on the sidebar</strong></p>
+        <p class="card-text">👈 Click on the sidebar</p>
       </div>
     </div>
   </div>
