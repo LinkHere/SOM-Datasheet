46b1ad197a91779cfaab6a42bb7cf0cbfc26a332
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-12 08:26:59 +0800

    Update Main_Page.py

--- Main/Main_Page.py
+++ Main/Main_Page.py
@@ -31,22 +31,61 @@ rows2 = run_query(f'SELECT Last_Name FROM "{sheet_url2}"')
 rows3 = run_query(f'SELECT Last_Name FROM "{sheet_url3}"')
 
 st.markdown(f"""
-    <div class="card" style="width: 18rem; color: #777;">
-        <div class="card-header">
-            Students
-        </div>
-        <ul class="list-group list-group-flush" style="color: #777;">
-            <li class="list-group-item">
-                <h6>1st Year <span class="badge bg-secondary">{len(rows1)}</span></h6>
-            </li>
-            <li class="list-group-item">
-                <h6>2nd Year <span class="badge bg-secondary">{len(rows2)}</span></h6>
-            </li>
-            <li class="list-group-item">
-                <h6>3rd Year <span class="badge bg-secondary">{len(rows3)}</span></h6>
-            </li>
-        </ul>
+#    <div class="card" style="width: 18rem; color: #777;">
+#        <div class="card-header">
+#            Students
+#        </div>
+#        <ul class="list-group list-group-flush" style="color: #777;">
+#            <li class="list-group-item">
+#                <h6>1st Year <span class="badge bg-secondary">{len(rows1)}</span></h6>
+#            </li>
+#            <li class="list-group-item">
+#                <h6>2nd Year <span class="badge bg-secondary">{len(rows2)}</span></h6>
+#            </li>
+#            <li class="list-group-item">
+#                <h6>3rd Year <span class="badge bg-secondary">{len(rows3)}</span></h6>
+#            </li>
+#        </ul>
+#    </div>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 """, unsafe_allow_html=True)
 
 st.markdown(f"""
