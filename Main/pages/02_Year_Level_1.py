426aa4c7c1367ef5dae7a3f661d4e10f6a5052ca
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-17 14:47:38 +0800

    Update 02_Year_Level_1.py

--- Main/pages/02_Year_Level_1.py
+++ Main/pages/02_Year_Level_1.py
@@ -37,9 +37,8 @@ for itrs, row in enumerate(rows, 1):
     else:
         btn_state = "disabled"
     st.markdown(f"""
-        <span class="badge text-bg-danger">itrs</span>
-    """,unsafe_allow_html=True)
-    student = st.checkbox(label=f'{row.Last_Name}, {row.First_Name} {row.Middle_Initial}', key=itrs)
+        <span class="badge text-bg-danger">{itrs}</span>
+    """,unsafe_allow_html=True) student = st.checkbox(label=f'{row.Last_Name}, {row.First_Name} {row.Middle_Initial}', key=itrs)
     if student:
         st.markdown(f"""
             <div class="card" style="color: #777;">
