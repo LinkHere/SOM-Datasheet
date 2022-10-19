0eecee4e01467a1990445c336a13363f227f6777
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-19 09:26:10 +0800

    Update 03_Year_Level_2.py

--- Main/pages/03_Year_Level_2.py
+++ Main/pages/03_Year_Level_2.py
@@ -42,36 +42,44 @@ for itrs, row in enumerate(rows, 1):
         btn_state = ""
     else:
         btn_state = "disabled"
+    
+    col1, col2, col3 = st.columns([0.3,3,2])
+    
+    with col1:
+        st.markdown(f"""<span class="badge text-bg-danger">{itrs}</span>""", unsafe_allow_html=True)
+    
+    with col2:
+        st.markdown(f"""<p>{row.Last_Name}, {row.First_Name} {row.Middle_Initial}</p>""", unsafe_allow_html=True)
         
-    st.markdown(f"""
-        <div class="card" style="margin-bottom: 2rem; color: #777;">
-         <div class="card-header">
-          Year Level 2
-         </div>
-
-         <div class="card-body">
-          <h5 class="card-title" style="color: #777"><strong style="color: #db0121">{itrs}</strong>|&nbsp;<u><i>{row.Last_Name}</i>, {row.First_Name} {row.Middle_Initial}</u></h5>
-                <em><p class="card-text"><strong>Permanent Address:</strong> {row.Permanent_Address}</br>
-                <strong>Current Address:</strong> {row.Current_Address}</br>
-                <strong>Staying with Relatives?:</strong> {row.Staying_with_relatives}</br>
-                <strong>Staying with other SOM Students?:</strong> {row.Staying_with_other_SOM_students}</br>
-                <strong>Staying with:</strong> {row.Staying_with}</br>
-                <strong>Father's Name and No.:</strong> {row.Father_No}</br>
-                <strong>Mother's Name and No.:</strong> {row.Mother_No}</br>
-                <strong>Emergency Contact Person:</strong> {row.Emergency_contact}</br>
-                <strong>CEU Mail:</strong> {row.CEU_Mail}</br>
-                <strong>Mobile No.:</strong> {mobile}</br>
-                <strong>PhilHealth?:</strong> {row.PhilHealth}</br>
-                <strong>PhilHealth Category:</strong> {row.PhilHealth_Category}</br>
-                <strong>Other Medical Insurance?:</strong> {row.Other_Medical_Insurance}</br>
-                <strong>Medical Insurances:</strong> {row.Medical_Insurance}</br>
-                <strong>Covid19 Vaccine?</strong> {row.Covid19_Vaccine}</br>
-                <strong>Covid19 Booster?</strong> {row.Covid19_Booster}</br>
-                </p></em>
-                <a href="{row.Vaccine_Id}" class="btn btn-outline-dark {btn_state}">Vaccination ID/Certificate</a>
-         </div>
-        </div>
-        """, unsafe_allow_html=True)
+    with col3:
+        student = st.checkbox('View Details', key=itrs)
+    
+    if student:
+        st.markdown(f"""
+            <div class="card" style="margin-bottom: 2rem; color: #777;">
+             <div class="card-body">
+              <h5 class="card-title" style="color: #777"><strong style="color: #db0121">{itrs}</strong>|&nbsp;<u><i>{row.Last_Name}</i>, {row.First_Name} {row.Middle_Initial}</u></h5>
+                    <em><p class="card-text"><strong>Permanent Address:</strong> {row.Permanent_Address}</br>
+                    <strong>Current Address:</strong> {row.Current_Address}</br>
+                    <strong>Staying with Relatives?:</strong> {row.Staying_with_relatives}</br>
+                    <strong>Staying with other SOM Students?:</strong> {row.Staying_with_other_SOM_students}</br>
+                    <strong>Staying with:</strong> {row.Staying_with}</br>
+                    <strong>Father's Name and No.:</strong> {row.Father_No}</br>
+                    <strong>Mother's Name and No.:</strong> {row.Mother_No}</br>
+                    <strong>Emergency Contact Person:</strong> {row.Emergency_contact}</br>
+                    <strong>CEU Mail:</strong> {row.CEU_Mail}</br>
+                    <strong>Mobile No.:</strong> {mobile}</br>
+                    <strong>PhilHealth?:</strong> {row.PhilHealth}</br>
+                    <strong>PhilHealth Category:</strong> {row.PhilHealth_Category}</br>
+                    <strong>Other Medical Insurance?:</strong> {row.Other_Medical_Insurance}</br>
+                    <strong>Medical Insurances:</strong> {row.Medical_Insurance}</br>
+                    <strong>Covid19 Vaccine?</strong> {row.Covid19_Vaccine}</br>
+                    <strong>Covid19 Booster?</strong> {row.Covid19_Booster}</br>
+                    </p></em>
+                    <a href="{row.Vaccine_Id}" class="btn btn-outline-dark {btn_state}">Vaccination ID/Certificate</a>
+             </div>
+            </div>
+            """, unsafe_allow_html=True)
 
 st.markdown(f"""
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
