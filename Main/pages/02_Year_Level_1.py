8ab39030581c7e226c37d21f963f350aeca633c4
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-19 09:08:13 +0800

    Update 02_Year_Level_1.py

--- Main/pages/02_Year_Level_1.py
+++ Main/pages/02_Year_Level_1.py
@@ -50,7 +50,7 @@ for itrs, row in enumerate(rows, 1):
     
     if student:
         st.markdown(f"""
-            <div class="card border-danger" style="margin-bottom: 10px; color: #777;">
+            <div class="card border-danger" style="margin-top: -10px; margin-bottom: 10px; color: #777;">
                 <div class="card-body">
                     <em><p class="card-text"><strong>Permanent Address:</strong> {row.Permanent_Address}</br>
                     <strong>Current Address:</strong> {row.Local_Address}</br>
