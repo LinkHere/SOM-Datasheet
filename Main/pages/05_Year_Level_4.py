8f47d10ccd70778a0a50e6eba70cbe26e0353869
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-20 12:24:09 +0800

    Update 05_Year_Level_4.py

--- Main/pages/05_Year_Level_4.py
+++ Main/pages/05_Year_Level_4.py
@@ -44,10 +44,10 @@ def show_details(Block, idx):
             {row.Last_Name}
         """)
             
-blck1a, blck1b, blck2a, blck2b = st.tabs(["Block1-A, Block1-B, Block2-A, Block2-B"])
+tab1, tab2, tab3, tab4 = st.tabs(["Block1-A, Block1-B, Block2-A, Block2-B"])
 idx = 1
 
-with blck1a:
+with tab1:
     show_details("1-A", idx)
 
 
