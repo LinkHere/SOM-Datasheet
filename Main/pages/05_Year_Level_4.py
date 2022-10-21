e6930674bf93e2ce017530ffe3ca7d6632404c64
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-21 11:32:54 +0800

    Update 05_Year_Level_4.py

--- Main/pages/05_Year_Level_4.py
+++ Main/pages/05_Year_Level_4.py
@@ -58,14 +58,12 @@ with blck1a:
         b1a = choose_block(sheet_url,"1A")
         show_details(b1a, idx)
     except:
-        st.markdown(f"""<p>kokokokokok</p>""", unsafe_allow_html=True)
     
 with blck1b:
     try:
         b1b = choose_block(sheet_url,"1B")
         show_details(b1b, idx)
     except:
-        st.write("adsfsdfsfdsfs")
             
 st.markdown(f"""
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
