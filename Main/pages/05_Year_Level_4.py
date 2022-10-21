2bcbc491fed9eefe78285e5a95a1c7ef1fafb3a9
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-10-21 08:53:18 +0800

    Update 05_Year_Level_4.py

--- Main/pages/05_Year_Level_4.py
+++ Main/pages/05_Year_Level_4.py
@@ -47,11 +47,11 @@ blck1a, blck1b, blck2a, blck2b, blck3a, blck3b, blck4a, blck4b, blck5a, blck5b =
 idx = 1
 
 with blck1a:
-    b1a = run_query(f'SELECT * FROM "{sheet_url} WHERE Block="1-A" "')
-    if b1a:
+    try:
+        b1a = run_query(f'SELECT * FROM "{sheet_url} WHERE Block="1-A" "')
         show_details(b1a, idx)
-    else:
-        st.write("Something went wrong!")
+    except:
+        st.write("WOOOOOOOOH, Something went wrong!")
     
 st.markdown(f"""
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
